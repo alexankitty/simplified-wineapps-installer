afe70bdb4e581c8222fadcce4a6470348fe27ba0
Alexandra <alexankitty@gmail.com>
2025-01-11 19:04:50 -0700

    gui setup template

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace simplified_wineapps_installer
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			new Application(Eto.Platform.Detect).Run(new MainForm());
+		}
+	}
+}
